97b43b7ab155718668ad7ff7afb1bab373afbdcd
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-03-22 17:42:11 +0100

    Modified 4 items, New 4 items

---
+++ client/mocks/sp_aptest.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.sp_aptest = window.mocks.sp_aptest || {};
--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.screen', {
+        views: {
+            app: {
+                controller: 'app_screen',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.screen');
+                }
+            }
+        }
     });
 });
