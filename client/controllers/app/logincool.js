17361f7dc726f43a2ccdc6761e6b7429d94d207f
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-03-27 10:15:55 +0100

    Modified 4 items, New 6 items

---
+++ client/controllers/app/logincool.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_logincool', app_logincool);
+function app_logincool($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.logincool', {
+        views: {
+            app: {
+                controller: 'app_logincool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.logincool');
+                }
+            }
+        }
     });
 });
