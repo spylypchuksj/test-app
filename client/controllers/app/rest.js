24da8307f31767e1730c198a562a6dc4c28d01d3
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-04-18 10:28:03 +0100

    New 2 items, Modified 3 items

---
+++ client/controllers/app/rest.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_rest', app_rest);
+function app_rest($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.rest', {
+        views: {
+            app: {
+                controller: 'app_rest',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.rest');
+                }
+            }
+        }
     });
 });
